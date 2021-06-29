ad45f8eacbbe03cad3da6908cd19a403383be4ee
Ali Sanad <alisanad099@gmail.com>
2021-06-29 06:33:48 +0200

    hello world from next js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+  return (
+    <>
+      <h1
+        style={{
+          backgroundColor: 'Cyan',
+          marginTop: '0px',
+          textAlign: 'center',
+          padding: '20px',
+        }}
+      >
+        HELLO THERE THIS IS A NAV BAR LOCATION
+      </h1>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
