d59239847a1f8a3328eca87fc2ff17750620bf4e
Ali Sanad <alisanad099@gmail.com>
2021-07-29 00:55:27 +0200

    single event ui

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,50 @@
 import Layout from '@/components/Layout';
 import {API_URL} from '@/config/index';
+import classes from '@/styles/Event.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import {FaPencilAlt, FaTimes} from 'react-icons/fa';
 const EventPage = ({evt}) => {
+  const deleteEvent = () => {
+    console.log('delete');
+  };
   return (
     <Layout>
-      <h1>{evt.name}</h1>
+      <div className={classes.event}>
+        <div className={classes.controls}>
+          <Link href={`/events/edit/${evt.id}`}>
+            <a>
+              <FaPencilAlt /> Edit Event
+            </a>
+          </Link>
+          <a href='#' className={classes.delete} onClick={() => deleteEvent()}>
+            <FaTimes /> Delete Event
+          </a>
+        </div>
+        <span>
+          {evt.date} at {evt.time}
+        </span>
+        <h1>{evt.name}</h1>
+        {evt.image && (
+          <div className={classes.image}>
+            <Image src={evt.image} width={960} height={600} alt='' />
+          </div>
+        )}
+        <h3>Performers:</h3>
+        <p>{evt.performers}</p>
+        <h3>Description:</h3>
+        <p>{evt.description}</p>
+        <h3>Venue: {evt.venue}</h3>
+        <p>{evt.address}</p>
+
+        <Link href='/events'>
+          <a className={classes.back}>{'<'} Go Back</a>
+        </Link>
+      </div>
     </Layout>
   );
 };
+
 //getStaticPaths => called at build time whenever dynamic routing exists
 export async function getStaticPaths() {
   const res = await fetch(`${API_URL}/api/events`);
